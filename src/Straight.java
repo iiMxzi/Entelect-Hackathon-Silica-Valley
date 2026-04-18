25a8ccafa262d33c3aa692e682901e13212af0db
Ayabonga Mazibuko <224011659@stu.ukzn.ac.za>
2026-04-18 14:00:40 +0200

    removed target_speed and braking_point since that needs to be done in the simulation

--- src/Straight.java
+++ src/Straight.java
@@ -1,22 +1,19 @@
 public class Straight extends Segment{
-    private final double target_speed;
-    private final double braking_point;
 
-
-    public Straight(int id, double length, String type, double tSpeed, double bPoint) {
+    /**
+     * @param id The segment ID from JSON
+     * @param length Total length in meters
+     * @param type Will be "straight"
+     */
+    public Straight(int id, double length, String type) {
         super(id, length, type);
-        this.target_speed = tSpeed;
-        this.braking_point = bPoint;
     }
 
-    public double getTargetSpeed() {
-        return target_speed;
-    }
-
-    public double getBrakingPoint() {
-        return braking_point;
-    }
-    //returns -1 since there is no entry speed for a staight
+    /**
+     * Straights do not have a physical grip limit like corners.
+     * We return -1 to signal to the Simulator that the only limit is the Car's max speed.
+     */
+    @Override
     public double getEntrySpeed(double friction, double crawlConstant) {
         return -1;
     }
