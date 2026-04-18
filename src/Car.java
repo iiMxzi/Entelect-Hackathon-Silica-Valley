376a103297361b86bc2adbfe50e1224cf7e9ea94
Ayabonga Mazibuko <224011659@stu.ukzn.ac.za>
2026-04-18 11:27:15 +0200

    added constructor and its parameter

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,22 @@
 public class Car {
+    private double max_speed_m_per_sec;
+    private final double ACCEL_M_PER_SEC2;
+    private final double BRAKE_M_PER_SEC2;
+    private final double CRAWL_CONSTANT_M_PER_SEC;
+
+    //TODO Complete documentation
+    /**
+     *
+     * @param mSpeed
+     * @param accelMPerSec2
+     * @param brakeMPerSec2
+     * @param crawlConstantMPerSec
+     */
+    public Car(double mSpeed,double accelMPerSec2, double brakeMPerSec2, double crawlConstantMPerSec) {
+        this.max_speed_m_per_sec = mSpeed;
+        this.ACCEL_M_PER_SEC2 = accelMPerSec2;
+        this.BRAKE_M_PER_SEC2 = brakeMPerSec2;
+        this.CRAWL_CONSTANT_M_PER_SEC = crawlConstantMPerSec;
+    }
+    
 }
